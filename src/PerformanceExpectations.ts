d3db497ffd14bc4b8e1f3c6eb88873dd552d508a
ethicalgaming <114638787+nexaod@users.noreply.github.com>
2022-10-01 05:00:50 +1000

    Add performance expectations and reformat leaderboard

--- src/PerformanceExpectations.ts
+++ src/PerformanceExpectations.ts
@@ -1,4 +1,5 @@
 import { Client, ClientOptions, SelectMenuOptionBuilder, TextChannel } from "discord.js";
+import { title } from "process";
 import { Colours, Emojis, Channels, Roles } from "./Utils";
 require('dotenv').config();
 
@@ -12,86 +13,74 @@ const client = new Client({
 
 client.login(token);
 
-const rangedData = require("./data/leaderboards/ranged.json");
-const meleeData = require("./data/leaderboards/melee.json");
-const magicData = require("./data/leaderboards/magic.json");
-const hybridData = require("./data/leaderboards/hybrid.json");
-
-const createLeaderboardArray = (data: any) => {
-    const finalArray = [];
-    for (let role in data) {
-        // Skip entire section if theres no data (useful when leaderboards are reset)
-        const scores = data[role].scores;
-        if (!scores[0].name) {
-            continue;
-        }
-        const roleObject: any = {};
-        roleObject.name = data[role].title;
-        roleObject.inline = false;
-        roleObject.value = `
-        ${scores[0].name ? `${Emojis.gem1} ${scores[0].name} - [${scores[0].score}](${scores[0].url})` : ''}
-        ${scores[1].name ? `${Emojis.gem2} ${scores[1].name} - [${scores[1].score}](${scores[1].url})` : ''}
-        ${scores[2].name ? `${Emojis.gem3} ${scores[2].name} - [${scores[2].score}](${scores[2].url})` : ''}
-        `
-        finalArray.push(roleObject)
-    }
-    return finalArray;
-}
-
-const createStyleEmbed = (data: any, title: string, colour: number, thumbnail: string) => {
-    return {
-        color: colour,
-        text_title: title,
-        thumbnail: {
-            url: thumbnail,
-        },
-        fields: createLeaderboardArray(data)
-    };
-}
-
 const embedsToSend: any = [];
-
-const rangedEmbed = createStyleEmbed(rangedData, 'Ranged', Colours.green, 'https://i.imgur.com/wvyFUc8.png')
-const meleeEmbed = createStyleEmbed(meleeData, 'Melee', Colours.red, 'https://puu.sh/JnDzI.png')
-const magicEmbed = createStyleEmbed(magicData, 'Magic', Colours.blue, 'https://i.imgur.com/pxUotNE.png')
-const hybridEmbed = createStyleEmbed(hybridData, 'Mage/Ranged', Colours.aqua, 'https://i.imgur.com/jR9vfjY.png')
-
-if (createLeaderboardArray(rangedData)) embedsToSend.push(rangedEmbed);
-if (createLeaderboardArray(meleeData)) embedsToSend.push(meleeEmbed);
-if (createLeaderboardArray(magicData)) embedsToSend.push(magicEmbed);
-if (createLeaderboardArray(hybridData)) embedsToSend.push(hybridEmbed);
-
 // const channel = client.channels.fetch(baseChannelID).then(channel => {
 //     channel?.send({ embeds: [exampleEmbed] });
 // }).catch(console.error);
 
-const startingMessage = `
-⬥ Players can only be on the leaderboard once per role, even if they would hold multiple spots on it (i.e. be #1 and #2).\n
-⬥ Only 2K Gems from **FULL** (7/7) FC teams are accepted. Gems will be denied for smaller team sizes.\n
-⬥ There must be at least 3 people from the team in the gem UNLESS a <@&${Roles.maintrialhost}>+ can vouch for the validity of the gem.\n
-⬥ Submitted role gems must at least qualify for the corresponding <@&${Roles.extreme}> role.
+const generalExpectations = `
+> These expectations apply to all roles and combat styles.\n
+⬥ A bank preset is required and **quick banking** is expected between kills. Slow banks are frowned upon and may result in retrial.\n
+⬥ The **Chinner** or **Base** is required to place the starting dummy.\n
+⬥ All ents are required to ${Emojis.vulnBomb} their ${Emojis.amalg}.\n
+⬥ You are required to ${Emojis.vulnBomb} your minion as ${Emojis.umbra} or ${Emojis.glacies} tank.\n
+⬥ ${Emojis.dommines} are not needed.\n
+⬥ Stall adrenaline before and between kills.\n
+⬥ Disassemble any dummies you use to build adrenaline.\n
+⬥ Regain adrenaline on your own if you died during a kill (preferably with Wars Retreat crystal).\n
+⬥ ${Emojis.ripperDemon}${Emojis.ripperScroll} is required with the exception of Ranged Base (${Emojis.mammoth}${Emojis.mammothScroll}), Magic Base (${Emojis.kalgDemon}${Emojis.kalgScroll}) and Ironmen Magic Roles (${Emojis.kalgDemon}${Emojis.kalgScroll}).\n
+⬥ Rotations are expected to be followed and can be found under **Guides**.\n
+⬥ If ${Emojis.kalgDemon}${Emojis.kalgScroll} is being used, you are expected to ${Emojis.callFamiliar} before speccing.\n
+⬥ If ${Emojis.ripperDemon}${Emojis.ripperScroll} is being used, you are expected to use ${Emojis.spiritualPrayer} and spec.\n
+⬥ Bombs are expected to be placed correctly and not affect teammates.\n
+⬥ 300K Minions are not acceptable. Doing this more than once in an hour will result in retrial.\n
+⬥ Minions are expected to be lured quickly, and fixed if mislured. A guide for both can be found in <#${Channels.additionalInfo}>.
 `
 
-const startingEmbed = {
-    color: 5198940,
-    title: "Nex AoD FC Leaderboards",
-    description: startingMessage
-};
+const magicEnt = `
+> A **5.5M** minimum damage threshold is expected in 30 minutes through a 2K Challenge Gem.\n
+⬥ ${Emojis.mani} is the only allowed aura.\n
+⬥ You can trial as either ${Emojis.umbra} or ${Emojis.glacies}.\n
+⬥ A 15% ${Emojis.detonate} (from losing target on ${Emojis.dummy}) is required on P2 to hit both ${Emojis.amalg} and ${Emojis.nex}.\n
+⬥ Do not stand melee distance of ${Emojis.umbra} pillar, as meleers might get hit by potential shadows.\n
+⬥ We strongly recommend you ${Emojis.magic}/${Emojis.melee} Hybrid.
+`
+
+const meleeEnt = `
+> A **5.1M** minimum damage threshold is expected in 30 minutes through a 2K Challenge Gem.\n
+⬥ ${Emojis.berserker} is the only allowed aura.\n
+⬥ You must trial as ${Emojis.glacies}.\n
+⬥ ${Emojis.greaterBarge} and ${Emojis.greaterFlurry} is required.\n
+⬥ It is highly recommended to utilise ${Emojis.scythe}${Emojis.hurricane} on P2 to hit both ${Emojis.amalg} and ${Emojis.nex}.\n
+⬥ It is highly recommended to utilise ${Emojis.scythe}${Emojis.cleave} on P3 to help clear minions.\n
+⬥ ${Emojis.dragonBattleAxe} at Adrenaline Crystal and be in time for P1.\n
+⬥ To unlock ${Roles.meleeUmbra} a trial is required, or ${Roles.meleeEnt} and Umbra in any other style.\n
+⬥ A knowledge of when to ${Emojis.zerk} is required (i.e. -1.2s).\n
+⬥ We strongly recommend you ${Emojis.magic}/${Emojis.melee} Hybrid.
+`
+
+const createEmbed = (data: string, colour: number, title: string) => {
+    return {
+        color: colour,
+        title: title,
+        description: data
+    };
+}
+
+const startingEmbed = createEmbed(generalExpectations, Colours.default, 'General Expectations');
+const magicEntEmbed = createEmbed(magicEnt, Colours.blue, `${Emojis.magic} Magic Entangle`);
+const meleeEntEmbed = createEmbed(meleeEnt, Colours.red, `${Emojis.melee} Melee Entangle`);
+
 
 client.on("ready", async () => {
     if (!client.user || !client.application) {
         return;
     }
-    client.channels.fetch(Channels.leaderboard)
+    client.channels.fetch(Channels.performanceExpectations)
         .then(async channel => {
             await (channel as TextChannel).bulkDelete(100)
-            await (channel as TextChannel).send({ embeds: [startingEmbed] })
-            for (let i = 0; i < embedsToSend.length; i++) {
-                await (channel as TextChannel).send(`> __**${embedsToSend[i].text_title}**__`)
-                await (channel as TextChannel).send({ embeds: [embedsToSend[i]] })
-            }
-            // await (channel as TextChannel).send({ embeds: embedsToSend })
-            await process.exit(1)
+            await (channel as TextChannel).send({ embeds: [startingEmbed, magicEntEmbed, meleeEntEmbed] })
+            await process.exit(0)
             // .then(() => (channel as TextChannel).send({ embeds: embedsToSend }))
             // .then(() => process.exit(1))
             // .catch(error => console.error);
--- src/Utils.ts
+++ src/Utils.ts
@@ -5,16 +5,21 @@ export const Colours = {
     red: 10038562,
     lightgrey: 10070709,
     gold: 12745742,
+    default: 5198940,
 }
 
 export const Channels = {
     leaderboard: '1024897631715069962',
-    applyforrank: '1025418108711731240',
+    applyForRank: '1025418108711731240',
+    performanceExpectations: '1024896851436122145',
+    additionalInfo: '1024897391549227038',
 }
 
 export const Roles = {
-    maintrialhost: '1024973897621315584',
-    extreme: '1024979670766194738'
+    mainTrialHost: '1024973897621315584',
+    extreme: '1024979670766194738',
+    meleeEnt: '<@&1025444893931352084>',
+    meleeUmbra: '<@&1025444129951449128>',
 }
 
 export const Emojis = {
@@ -28,5 +33,30 @@ export const Emojis = {
     glacies: '<:glacies:1025086094775177297>',
     cruor: '<:cruor:1025085871772409937>',
     fumus: '<:fumus:1025086111262982237>',
-    freedom: '<:freedom:535541258240786434>'
+    freedom: '<:freedom:535541258240786434>',
+    dommines: '<:dommine:662249620579155968>',
+    ripperDemon: '<:ripperpouch:703581275453128714>',
+    ripperScroll: '<:ripperscroll:703581275155464203>',
+    kalgDemon: '<:kalgpouch:921410226484301925>',
+    kalgScroll: '<:kalgscroll:841409588954923049>',
+    mammoth: '<:Mammoth:513195712146702337>',
+    mammothScroll: '<:mammothscroll:1025428178111578182>',
+    callFamiliar: '<:callfollower:933299003121078332>',
+    spiritualPrayer: '<:spiritualprayer:651079281882955787>',
+    mani: '<:mani:643505153709965322>',
+    berserker: '<:berserker:643505116347105290>',
+    vulnBomb: '<:vulnbomb:655341074235129858>',
+    amalg: '<:shadowamalg:878739232951255091>',
+    magic: '<:Magic:689504724159823906>',
+    melee: '<:melee:615612332521029632>',
+    dummy: '<:dummy:656844963522281473>',
+    nex: '<:aod:580167371365548042>',
+    detonate: '<:deto:535533833358016512>',
+    scythe: '<:noxscythe:513190159341322240>',
+    hurricane: '<:cane:535532878969438210>',
+    greaterBarge: '<:gbarge:535532879250456578>',
+    greaterFlurry: '<:gflurry:535532879283879977>',
+    dragonBattleAxe: '<:DBA:603979368850653216>',
+    cleave: '<:cleave:535532878616985610>',
+    zerk: '<:zerk:535532854004678657>'
 }
