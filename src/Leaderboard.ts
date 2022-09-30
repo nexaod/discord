ea71839bd5a8168fd84d02b4fb9ee2299b0638d7
nexaod <114638787+nexaod@users.noreply.github.com>
2022-10-01 00:45:04 +1000

    Update Leaderboard.ts

--- src/Leaderboard.ts
+++ src/Leaderboard.ts
@@ -112,10 +112,15 @@ embedsToSend.push(createKillTimeEmbed(killTimeData))
 // }).catch(console.error);
 
 const startingMessage = `
+> __**Gem Scores**__
 ⬥ Players can only be on the leaderboard once per role, even if they would hold multiple spots on it (i.e. be #1 and #2).\n
 ⬥ Only 2K Gems from **FULL** (7/7) FC teams are accepted. Gems will be denied for smaller team sizes.\n
 ⬥ There must be at least 3 people from the team in the gem UNLESS a <@&${Roles.maintrialhost}>+ can vouch for the validity of the gem.\n
 ⬥ Submitted role gems must at least qualify for the corresponding <@&${Roles.extreme}> role.
+> __**Kill Times**__ 
+⬥ Only full (7/7) FC teams count.
+⬥ A screenshot of the full client interface with the group interface with 7/7 members showing is required.
+⬥ You can apply for a spot on the leaderboard via the #apply-for-rank channel.
 `
 
 const startingEmbed = {
