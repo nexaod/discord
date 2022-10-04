7dcf10c2cfe1490cae015081e26d4586674ddfd2
ethicalgaming <114638787+nexaod@users.noreply.github.com>
2022-10-04 17:37:28 +1100

    Update AdditionalInfo.ts

--- src/data/AdditionalInfo.ts
+++ src/data/AdditionalInfo.ts
@@ -3,7 +3,9 @@ export const general = `
 ⬥ [Trial Schedule](https://docs.google.com/spreadsheets/d/1_kF9Dmk77avHdjbl2zyTSk0WAEQWlVrlP12KxvP_2-4/edit?usp=sharing)\n
 ⬥ [Report a Player](https://forms.gle/KjFnTvG88WEmgjUJ8)\n
 ⬥ [Scammer List](https://docs.google.com/spreadsheets/d/104eda-Z41J_HCo2tc9cDwRHphp2pYW0YzuKShM3HYTk/edit?usp=sharing)\n
-⬥ [W2 Split Calculator](https://docs.google.com/spreadsheets/d/1Bts8t65HXtcc9YdOnwpiUTwTxl5YBYRfN6mpwwBRMoI/edit#gid=0)
+⬥ [W2 Split Calculator](https://docs.google.com/spreadsheets/d/1Bts8t65HXtcc9YdOnwpiUTwTxl5YBYRfN6mpwwBRMoI/edit#gid=0)\n
+⬥ [Hybrid Action Bar Binding](https://www.youtube.com/watch?v=-LCye6eJhO0&t=1s)\n
+⬥ [Efficient Banking](https://www.youtube.com/watch?v=inQbe28eS9c)\n
 `;
 
 export const inviteURL = 'https://discord.gg/Q6zMtwKEgV';
