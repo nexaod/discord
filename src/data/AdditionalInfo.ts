66956ab9e293cafbfe5817dc9056163a60e514a9
ethicalgaming <114638787+nexaod@users.noreply.github.com>
2022-10-05 17:51:53 +1100

    Add FC Feedback

--- src/data/AdditionalInfo.ts
+++ src/data/AdditionalInfo.ts
@@ -2,6 +2,7 @@ export const general = `
 > Some helpful links and tools.\n
 ⬥ [Trial Schedule](https://docs.google.com/spreadsheets/d/1_kF9Dmk77avHdjbl2zyTSk0WAEQWlVrlP12KxvP_2-4/edit?usp=sharing)\n
 ⬥ [Report a Player](https://forms.gle/KjFnTvG88WEmgjUJ8)\n
+⬥ [FC Feedback](https://forms.gle/KMfHb4uCXhX64MN96)\n
 ⬥ [Scammer List](https://docs.google.com/spreadsheets/d/104eda-Z41J_HCo2tc9cDwRHphp2pYW0YzuKShM3HYTk/edit?usp=sharing)\n
 ⬥ [W2 Split Calculator](https://docs.google.com/spreadsheets/d/1Bts8t65HXtcc9YdOnwpiUTwTxl5YBYRfN6mpwwBRMoI/edit#gid=0)\n
 ⬥ [Hybrid Action Bar Binding](https://www.youtube.com/watch?v=-LCye6eJhO0&t=1s)\n
