45518e244cc7583d4dd6bdad434aac14b5e41e4f
ethicalgaming <114638787+nexaod@users.noreply.github.com>
2022-10-19 15:29:43 +1100

    Update magic guides

--- src/data/guides/magic/magicBase.ts
+++ src/data/guides/magic/magicBase.ts
@@ -5,29 +5,28 @@ export const magicBasePreset = 'https://cdn.discordapp.com/attachments/898264948
 export const magicBasePresetInfo = `
 > Courtesy of **Stuman131** and **12dragon162**\n
 ⬥ ${Emojis.eofPurple} ${Emojis.gstaff} Guthix Staff
-⬥ Pouches should contain runes for ${Emojis.inciteFear} ${Emojis.exsang} ${Emojis.bloodBarrage} ${Emojis.smokecloud}.
-⬥ If using ${Emojis.berserkersFury}, you can consider using ${Emojis.iceBarrage} to keep lifepoints low.
-⬥ If using ${Emojis.meta} on **Phase 3 and 4**, replace all instances of ${Emojis.bloodBarrage} with ${Emojis.iceBarrage}.
+⬥ Pouches should contain runes for ${Emojis.inciteFear} ${Emojis.exsang} ${Emojis.iceBarrage} ${Emojis.smokecloud}.
 ⬥ Hybrid Basing with ${Emojis.magic} + ${Emojis.melee} is highly recommended due to fast modern speeds.
 `;
 
 export const magicBaseRotation = [`
 __**Pre Build**__\n
 > Give ${Emojis.dbreath} as indication ability. If both ${Emojis.magic} + ${Emojis.melee} and ${Emojis.magic} + ${Emojis.ranged} are on team, give ${Emojis.gconc} ${Emojis.dbreath}. Consider using defensives to save ${Emojis.adrenRenewal4}. Start with ${Emojis.inciteFear}.\n
-${Emojis.greaterSun} + ${Emojis.adrenRenewal4} → 2 Abilities → ${Emojis.bloodBarrage} + ${Emojis.fsoa} + ${Emojis.spec} → Build to 100 and lose target → ${Emojis.prep} → ${Emojis.magma} → ${Emojis.detonate}\n
+${Emojis.greaterSun} + ${Emojis.adrenRenewal4} → 2 Abilities → ${Emojis.iceBarrage} + ${Emojis.fsoa} + ${Emojis.spec} → Build to 100 and lose target → ${Emojis.prep} → ${Emojis.magma} → ${Emojis.detonate}\n
 __**Phase 1**__\n
 Target Cycle ${Emojis.nex} + ${Emojis.smokecloud} + ${Emojis.gstaff} ${Emojis.eofSpec} → ${Emojis.omni} → ${Emojis.limitless} + ${Emojis.smokeTendril} → ${Emojis.wildMagic} → ${Emojis.gconc} → ${Emojis.dbreath}\n
 __**Phase 2**__\n
-${Emojis.shard} → ${Emojis.magma} ${Emojis.cruor} → ${Emojis.divert} → ${Emojis.combust} → ${Emojis.bloodBarrage} + ${Emojis.gconc} → Target ${Emojis.iceamalg} + ${Emojis.nami} + ${Emojis.dbreath} → Target ${Emojis.bloodamalg} + ${Emojis.bloodBarrage} + ${Emojis.greaterChain} → ${Emojis.gconc} → ${Emojis.magma}\n
+${Emojis.shard} → ${Emojis.magma} ${Emojis.cruor} → ${Emojis.divert} → ${Emojis.combust} → ${Emojis.iceBarrage} + ${Emojis.gconc} → Target ${Emojis.iceamalg} + ${Emojis.nami} + ${Emojis.dbreath} → Target ${Emojis.bloodamalg} + ${Emojis.iceBarrage} + ${Emojis.greaterChain} → ${Emojis.gconc} → ${Emojis.magma}\n
 `,
 `__**Phase 3**__\n
-${Emojis.greaterSun} / ${Emojis.meta} Switch spells to ${Emojis.bloodBarrage} → ${Emojis.bloodBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → Switch spell to ${Emojis.inciteFear} + ${Emojis.bloodBarrage} + ${Emojis.fsoa} ${Emojis.spec} → Switch spell to ${Emojis.bloodBarrage} + ${Emojis.gconc} → ${Emojis.shatter} → ${Emojis.wildMagic} → ${Emojis.smokeTendril} → ${Emojis.omni} → ${Emojis.asphyx} (3 Tick) → ${Emojis.gstaff} ${Emojis.eofSpec}\n
+${Emojis.greaterSun} / ${Emojis.meta} Switch spells to ${Emojis.iceBarrage} → ${Emojis.iceBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → Switch spell to ${Emojis.inciteFear} + ${Emojis.iceBarrage} + ${Emojis.fsoa} ${Emojis.spec} → Switch spell to ${Emojis.iceBarrage} + ${Emojis.gconc} → ${Emojis.shatter} → ${Emojis.wildMagic} → ${Emojis.smokeTendril} → ${Emojis.omni} → ${Emojis.asphyx} (3 Tick) → ${Emojis.gstaff} ${Emojis.eofSpec}\n
 __**Pillars**__\n
 > Lure the boss, ${Emojis.hammer} and build to 100.\n
 **Pillar 4** -  ${Emojis.smokecloud} + ${Emojis.nami} → ${Emojis.gconc} → ${Emojis.dbreath}\n
 `,
 `__**Zaros**__\n
-Target Cycle ${Emojis.nex} + ${Emojis.meta} + ${Emojis.adrenRenewal4} → ${Emojis.bloodBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.bloodBarrage} + ${Emojis.omni} → ${Emojis.fsoa} ${Emojis.spec} → ${Emojis.limitless} + ${Emojis.smokeTendril} → ${Emojis.wildMagic} → ${Emojis.gstaff} ${Emojis.eofSpec}
+> On faster kills this rotation is not possible. Use good abilities and dump adrenaline on this phase.\n
+Target Cycle ${Emojis.nex} + ${Emojis.meta} + ${Emojis.adrenRenewal4} → ${Emojis.iceBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.iceBarrage} + ${Emojis.omni} → ${Emojis.fsoa} ${Emojis.spec} → ${Emojis.limitless} + ${Emojis.smokeTendril} → ${Emojis.wildMagic} → ${Emojis.gstaff} ${Emojis.eofSpec}
 `
 ];
 
--- src/data/guides/magic/magicEntangle.ts
+++ src/data/guides/magic/magicEntangle.ts
@@ -8,9 +8,7 @@ export const magicEntPresetInfo = `
 ⬥ ${Emojis.eofYellow} ${Emojis.abs} Armadyl Battlestaff
 ⬥ ${Emojis.sgbEof} can be replaced with ${Emojis.ecb}, ${Emojis.sgbEof}${Emojis.eof} and ${Emojis.quiver} containing ${Emojis.rubyBak}.
 ⬥ ${Emojis.eofYellow} ${Emojis.abs} Armadyl Battlestaff is not needed in faster teams.
-⬥ Pouches should contain runes for ${Emojis.inciteFear} ${Emojis.exsang} ${Emojis.bloodBarrage} ${Emojis.smokecloud}.
-⬥ If using ${Emojis.berserkersFury}, you can consider using ${Emojis.iceBarrage} to keep lifepoints low.
-⬥ If using ${Emojis.meta} on **Phase 3 and 4**, replace all instances of ${Emojis.bloodBarrage} with ${Emojis.iceBarrage}.
+⬥ Pouches should contain runes for ${Emojis.inciteFear} ${Emojis.exsang} ${Emojis.iceBarrage} ${Emojis.smokecloud}.
 ⬥ For kills that are faster than **1:46**, on **Pillar 4** and **Zaros**:
 ${TextUtils.noSpaceSubPoint} Cooldowns for ${Emojis.nami} ${Emojis.meta} and ${Emojis.fsoa} will not be available.
 ${TextUtils.noSpaceSubPoint} Improvisation is needed depending on speed.
@@ -21,23 +19,24 @@ ${TextUtils.noSpaceSubPoint} We highly recommend switching to a Hybrid style suc
 export const magicEntRotation = [`
 __**Pre Build**__\n
 > See base for indication ability ${Emojis.gconc} or ${Emojis.dbreath}. Consider using defensives to save ${Emojis.adrenRenewal4}. Start with ${Emojis.inciteFear}.\n
-${Emojis.greaterSun} + ${Emojis.adrenRenewal4} → ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.bloodBarrage} + ${Emojis.fsoa} + ${Emojis.spec} → Build to 100 and get ${Emojis.greaterFury}\n
+${Emojis.greaterSun} + ${Emojis.adrenRenewal4} → ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.iceBarrage} + ${Emojis.fsoa} + ${Emojis.spec} → Build to 100 and get ${Emojis.greaterFury}\n
 __**Phase 1**__\n
 ${Emojis.magma} (at 2.4s) → Stall ${Emojis.omni} → Target Cycle ${Emojis.nex} + ${Emojis.smokecloud} + ${Emojis.smokeTendril} → ${Emojis.wildMagic} → ${Emojis.asphyx} / ${Emojis.gconc} if low adren → ${Emojis.dbreath}\n
 __**Phase 2**__\n
-${Emojis.shard} → Target ${Emojis.dummy} → ${Emojis.sonic} → ${Emojis.anticipate} → ${Emojis.magma} → Lose Target +  ${Emojis.detonate} → Target Cycle ${Emojis.nex} → ${Emojis.bloodBarrage} + ${Emojis.detonate} + ${Emojis.gstaff} ${Emojis.eofSpec} → ${Emojis.nami} → ${Emojis.dbreath} → Target ${Emojis.amalg} → ${Emojis.gconc} → Target Cycle ${Emojis.nex} + ${Emojis.greaterChain} → ${Emojis.gconc} → ${Emojis.surge} → ${Emojis.magma}\n
+${Emojis.shard} → Target ${Emojis.dummy} → ${Emojis.sonic} → ${Emojis.anticipate} → ${Emojis.magma} → Lose Target +  ${Emojis.detonate} → Target Cycle ${Emojis.nex} → ${Emojis.iceBarrage} + ${Emojis.detonate} + ${Emojis.gstaff} ${Emojis.eofSpec} → ${Emojis.nami} → ${Emojis.dbreath} → Target ${Emojis.amalg} → ${Emojis.gconc} → Target Cycle ${Emojis.nex} + ${Emojis.greaterChain} → ${Emojis.gconc} → ${Emojis.surge} → ${Emojis.magma}\n
 `,
 `__**Phase 3**__\n
-${Emojis.greaterSun} / ${Emojis.meta} Switch spells to ${Emojis.bloodBarrage} → ${Emojis.bloodBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → Switch spell to ${Emojis.inciteFear} + ${Emojis.bloodBarrage} + ${Emojis.fsoa} ${Emojis.spec} → Switch spell to ${Emojis.bloodBarrage} + ${Emojis.gconc} → ${Emojis.omni} → ${Emojis.wildMagic} → ${Emojis.asphyx} (3 Tick) → ${Emojis.smokeTendril} → ${Emojis.gstaff} ${Emojis.eofSpec}\n
+${Emojis.greaterSun} / ${Emojis.meta} Switch spells to ${Emojis.iceBarrage} → ${Emojis.iceBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → Switch spell to ${Emojis.inciteFear} + ${Emojis.iceBarrage} + ${Emojis.fsoa} ${Emojis.spec} → Switch spell to ${Emojis.iceBarrage} + ${Emojis.gconc} → ${Emojis.omni} → ${Emojis.wildMagic} → ${Emojis.asphyx} (3 Tick) → ${Emojis.smokeTendril} → ${Emojis.gstaff} ${Emojis.eofSpec}\n
 __**Pillars**__\n
 > Improvise pillars if not dead. Flank accordingly, U/F pillars. Rotation assumes ${Emojis.umbra}${Emojis.glacies}${Emojis.cruor}${Emojis.fumus}\n
 **Pillar 1** -  ${Emojis.gconc} → ${Emojis.flankIcon} ${Emojis.deepImpact} → ${Emojis.gstaff} ${Emojis.eofSpec} → ${Emojis.gconc} → ${Emojis.flankIcon} ${Emojis.impact}\n
 **Pillar 2** -  ${Emojis.wildMagic} → ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.asphyx} → ${Emojis.wrackAndRuin}\n
-**Pillar 3** -  ${Emojis.sgbEof} ${Emojis.spec} → ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.bloodBarrage} + ${Emojis.gstaff} + ${Emojis.eofSpec} → ${Emojis.gconc} → ${Emojis.tuska}\n
-**Pillar 4** -  ${Emojis.bloodBarrage} + ${Emojis.nami} → ${Emojis.gconc} → ${Emojis.flankIcon} ${Emojis.impact}\n
+**Pillar 3** -  ${Emojis.sgbEof} ${Emojis.spec} → ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.iceBarrage} + ${Emojis.gstaff} + ${Emojis.eofSpec} → ${Emojis.gconc} → ${Emojis.tuska}\n
+**Pillar 4** -  ${Emojis.iceBarrage} + ${Emojis.nami} → ${Emojis.gconc} → ${Emojis.flankIcon} ${Emojis.impact}\n
 `,
 `__**Zaros**__\n
-${Emojis.magma} → ${Emojis.meta} + ${Emojis.adrenRenewal4} → ${Emojis.bloodBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.omni} → ${Emojis.fsoa} ${Emojis.spec} → ${Emojis.limitless} + ${Emojis.smokeTendril} → ${Emojis.wildMagic}
+> On faster kills this rotation is not possible. Use good abilities and dump adrenaline on this phase.\n
+${Emojis.magma} → ${Emojis.meta} + ${Emojis.adrenRenewal4} → ${Emojis.iceBarrage} + ${Emojis.gconc} → ${Emojis.dbreath} → ${Emojis.omni} → ${Emojis.fsoa} ${Emojis.spec} → ${Emojis.limitless} + ${Emojis.smokeTendril} → ${Emojis.wildMagic}
 `
 ];
 
