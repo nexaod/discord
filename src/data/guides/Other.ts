efaebcb500cfef2c72debdd0318fb9a2eeced2f9
Nex AoD <114638787+nexaod@users.noreply.github.com>
2023-01-01 14:39:24 +0800

    Update other guides to have complete pillar guide

--- src/data/guides/Other.ts
+++ src/data/guides/Other.ts
@@ -1,8 +1,9 @@
 export const general = `
 > Additional guides and rotations.\n
+⬥ [Complete Pillar Guide](https://www.youtube.com/watch?v=5mDfGRW2JB0)\n
 ⬥ [Efficient Banking](https://www.youtube.com/watch?v=inQbe28eS9c)\n
-⬥ [Luring Nex as Base](https://docs.google.com/document/d/17kRtNQsRLPcukH-6Y_PTPK9FutPSvK83a7k0ho6TVeU/edit) (Courtesy of **Logging in**)\n
-⬥ [Vulning Pillars](https://www.youtube.com/watch?v=HmYN8WGTbXA) (Courtesy of **Kaigi**)\n
+⬥ [Luring Nex as Base](https://docs.google.com/document/d/17kRtNQsRLPcukH-6Y_PTPK9FutPSvK83a7k0ho6TVeU/edit)\n
+⬥ [Vulning Pillars](https://www.youtube.com/watch?v=HmYN8WGTbXA)\n
 ⬥ [Reluring Minions](https://media.discordapp.net/attachments/1027186342620299315/1032892341062746112/unknown.png)\n
 ⬥ [Role Improvement Tips](https://docs.google.com/document/d/1nxrEGtKRdTHk9eUWG2pgqWXFoRie9jXtIs-1MkhTuPc/edit#)\n
 `;
