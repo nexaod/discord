ec1578ce021aff0baadb05b76ee824e67ef6f164
Fate <114638787+nexaod@users.noreply.github.com>
2023-10-23 21:42:16 +1100

    Update Leaderboard.ts

--- src/data/leaderboards/Leaderboard.ts
+++ src/data/leaderboards/Leaderboard.ts
@@ -7,6 +7,7 @@ export const startingMessage = `
 ⬥ Only 2K Gems from 7/7 FC teams are accepted **OR** 5+/7 members. Gems will be denied for smaller team sizes.\n
 ⬥ If the team is not full 7/7 FC, each non-FC member must join the challenge gem, and must hit the entry requirement for their role.\n
 ⬥ There must be at least 3 people from the team in the gem UNLESS a ${Roles.mainTrialHost}+ can vouch for the validity of the gem.\n
+⬥ The screenshot must contain codeword, summoning interface, group and challenge gem.\n
 ⬥ Submitted role gems must at least qualify for the corresponding ${Roles.extreme} role.\n
 > __**Kill Times**__\n
 ⬥ All submissions must be full FC teams (7/7).\n
